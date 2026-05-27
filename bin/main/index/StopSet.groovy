224d2785897e74ffdb88943871ab325cc55b4c92
aceslh-laurie <aceslh@hallam.ac.uk>
2026-05-27 10:31:07 +0100

    remove old

--- bin/main/index/StopSet.groovy
+++
@@ -1,9 +0,0 @@
-package index
-class StopSet {
-
-    public static Set<String> getStopSetFromFile() {
-
-        Set<String> stopSet = new File('src/cfg/stop_words_moderate.txt') as Set<String>
-        return stopSet.asImmutable()
-    }
-}
